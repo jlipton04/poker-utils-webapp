f218d4d150a93d4f30f0626ab5e676621b4cdfb3
Jacob Lipton <jlipton@clemson.edu>
2023-06-19 19:32:01 -0400

    Added basic information to the options bar

--- app/components/optionsBar/index.tsx
+++ app/components/optionsBar/index.tsx
@@ -2,10 +2,16 @@
 
 import './styles.css'
 
-export default function OptionsBar() {
+export type OptionsBarProps = {
+    position: string,
+    mode: string
+}
+
+export default function OptionsBar(props: OptionsBarProps) {
     return (
         <div className="optionsBar">
-            
+            <div className="position">Postition: <span>{props.position}</span></div>
+            <div className="mode">Mode: <span>{props.mode}</span></div>
         </div>
     )
 }

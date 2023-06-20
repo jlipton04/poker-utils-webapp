77ae8c62236e3903c4e38610cc26cf4e81c56811
Jacob Lipton <jlipton@clemson.edu>
2023-06-19 20:41:39 -0400

    Added functionality for rangeStore and RFI action checking

---
+++ lib/rangeStore.tsx
@@ -0,0 +1,75 @@
+import fs from 'fs'
+import path from 'path'
+
+const RANGE_DIR = 'ranges'
+
+const MODES = [
+    'RFI'
+]
+
+const POSITIONS = [
+    'UTG',
+    'UTG+1',
+    'UTG+2',
+    'LJ',
+    'HJ',
+    'CO',
+    'BTN',
+    'SB'
+]
+
+const ACTIONS = [
+    'RAISE',
+    'CALL'
+]
+
+const loadedRanges: Map<string, Map<string, Map<string, string[]>>> = new Map<string, Map<string, Map<string, string[]>>>()
+
+export function getLoadedRanges() {
+    if (loadedRanges.size === 0) {
+        loadRangesFromFile()
+    }
+
+    return loadedRanges
+}
+
+function loadRangesFromFile() {
+    MODES.forEach((mode) => {
+        POSITIONS.forEach((position) => {
+            ACTIONS.forEach((action) => {
+                loadRangeFromFile(mode, position, action)
+            })
+        })
+    })
+}
+
+function loadRangeFromFile(
+    mode: string,
+    position: string,
+    action: string
+) {
+    const filePath = path.join(process.cwd(), `${RANGE_DIR}/${mode}`, `${position}_${action}`)
+
+    if (!fs.existsSync(filePath)) {
+        return
+    }
+
+    const fileContent = fs.readFileSync(filePath, 'utf-8')
+    const parsedWithPercentages = fileContent.split(',')
+
+    if (mode == 'RFI') {
+        parsedWithPercentages.forEach((card, index) => {
+            parsedWithPercentages[index] = card.substring(0, card.indexOf(':'))
+        })
+    }
+
+    if (!loadedRanges.has(mode)) {
+        loadedRanges.set(mode, new Map<string, Map<string, string[]>>())
+    }
+
+    if (!loadedRanges.get(mode)!!.has(position)) {
+        loadedRanges.get(mode)!!.set(position, new Map<string, string[]>())
+    }
+
+    loadedRanges.get(mode)!!.get(position)!!.set(action, parsedWithPercentages)
+}
--- pages/api/isCorrectAction.tsx
+++ pages/api/isCorrectAction.tsx
@@ -1,8 +1,5 @@
-import fs from 'fs'
-import path from 'path'
+import { getLoadedRanges } from '../../lib/rangeStore'
 import type { NextApiRequest, NextApiResponse } from 'next'
-
-const RANGE_DIR = 'ranges'
 
 export type IsCorrectActionRequest = {
     mode: string,
@@ -15,8 +12,6 @@ export type IsCorrectActionResponse = {
     correct: boolean
 }
 
-const loadedRanges: Map<string, Map<string, Map<string, string[]>>> = new Map<string, Map<string, Map<string, string[]>>>()
-
 export default function handler(
     req: NextApiRequest,
     res: NextApiResponse<IsCorrectActionResponse>
@@ -25,35 +20,40 @@ export default function handler(
 
     const { mode, position, cardQuality, action } = req.body as IsCorrectActionRequest
 
-    if (!loadedRanges.get(mode)?.get(position)?.get(action)) {
-        loadRange(mode, position, action)
+    let answer: boolean = false;
+
+    if (mode === 'RFI') {
+        answer = isCorrectRfiAction(position, cardQuality, action)
     }
 
-    const currentRange = loadedRanges.get(mode)!!.get(position)!!.get(action)!!
-
     res.status(200).json({
-        correct: currentRange.includes(cardQuality)
+        correct: answer
     })
 }
 
-function loadRange(mode: string, position: string, action: string) {
-    const filePath = path.join(process.cwd(), RANGE_DIR, `${mode}_${position}_${action}.txt`)
-    const fileContent = fs.readFileSync(filePath, 'utf-8')
-    const parsedWithPercentages = fileContent.split(',')
+function isCorrectRfiAction(
+    position: string,
+    cardQuality: string,
+    action: string
+): boolean {
+    const ranges = getLoadedRanges().get('RFI')!!.get(position)!!
 
-    if (mode == 'RFI') {
-        parsedWithPercentages.forEach((card, index) => {
-            parsedWithPercentages[index] = card.substring(0, 3)
-        })
+    if (action === 'RAISE') {
+        return ranges.get('RAISE')!!.includes(cardQuality)
     }
 
-    if (!loadedRanges.has(mode)) {
-        loadedRanges.set(mode, new Map<string, Map<string, string[]>>())
+    if (action === 'CALL') {
+        const callRange = ranges.get('CALL')
+
+        return callRange?.includes(cardQuality) ?? false
     }
 
-    if (!loadedRanges.get(mode)!!.has(position)) {
-        loadedRanges.get(mode)!!.set(position, new Map<string, string[]>())
+    if (action === 'FOLD') {
+        const raiseRange = ranges.get('RAISE')!!
+        const callRange = ranges.get('CALL')
+
+        return !raiseRange.includes(cardQuality) && (!callRange?.includes(cardQuality))
     }
 
-    loadedRanges.get(mode)!!.get(position)!!.set(action, parsedWithPercentages)
+    return false
 }

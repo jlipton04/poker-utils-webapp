4be37e61a4b1f39ccade7066e1f7731a3046d83e
Jacob Lipton <jacoblipton4@gmail.com>
2023-07-06 21:25:58 -0700

    Added decentralizedBanker function and unit tests

---
+++ lib/decentrailizedBanker.ts
@@ -0,0 +1,56 @@
+import { Heap } from 'heap-js'
+
+export type Transaction = {
+    from: number,
+    to: number,
+    amount: number
+}
+
+type PlayerBalance = {
+    id: number
+    balance: number
+}
+
+const customComparator = (a: PlayerBalance, b: PlayerBalance) => b.balance - a.balance
+
+/**
+ * If the balances do not sum to zero, the function will return null
+ * 
+ * @param balances List of balances of each player; The indicies will be used as player IDs
+ * @returns Minimum list of transactions required to make all parties whole
+ */
+export function getMinTransfers(balances: number[]): Transaction[] | null {
+    if (balances.reduce((sum, currBal) => sum + currBal) != 0) {
+        return null
+    }
+
+    const transactions: Transaction[] = []
+
+    // Initialize heap with only non-zero balances
+    const balanceHeap = new Heap(customComparator)
+    balanceHeap.init(balances
+        .filter(balance => balance !== 0)
+        .map((balance, id) => ({balance, id}))
+    )
+
+    while (balanceHeap.size() > 1) {
+        const biggest = balanceHeap.pop()!
+        const smallest = balanceHeap.bottom(1)[0]
+
+        transactions.push({
+            from: smallest.id,
+            to: biggest.id,
+            amount: Math.abs(smallest.balance)
+        })
+
+        balanceHeap.remove(smallest)
+
+        biggest.balance = biggest.balance + smallest.balance
+
+        if (biggest.balance !== 0) {
+            balanceHeap.push(biggest)
+        }
+    }
+
+    return transactions
+}
---
+++ tst/decentrailizedBanker.test.ts
@@ -0,0 +1,45 @@
+import { Transaction, getMinTransfers } from "../lib/decentrailizedBanker"
+
+describe('getMinTransfers tests', () => {
+    it('should return an empty array if balances are already zero', () => {
+        const balances = [0, 0, 0]
+        const transfers = getMinTransfers(balances)
+        expect(transfers).toEqual([])
+      })
+    
+      it('should return the correct transfers for balances with both positive and negative values', () => {
+        const balances = [-1, -1, 2]
+        const transfers = getMinTransfers(balances)
+        expect(transfers).toEqual(expect.arrayContaining([
+            createTransaction(0, 2, 1),
+            createTransaction(1, 2, 1)
+        ] as Transaction[]))
+      })
+
+      it('should return the correct transfers with big loser', () => {
+        const balances = [-3, 1, 2]
+        const transfers = getMinTransfers(balances)
+        expect(transfers).toEqual(expect.arrayContaining([
+            createTransaction(0, 2, 3),
+            createTransaction(2, 1, 1)
+        ] as Transaction[]))
+      })
+    
+      it('should return null if it is not possible to bring balances to zero', () => {
+        const balances = [1, 1, 1]
+        const transfers = getMinTransfers(balances)
+        expect(transfers).toBeNull()
+      })
+})
+
+function createTransaction(
+    from: number,
+    to: number,
+    amount: number
+) {
+    return {
+        from,
+        to,
+        amount
+    } as Transaction
+}

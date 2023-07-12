39fc6b249fdbdf3d83f146531757c7c441fcfc12
Jacob Lipton <jacoblipton4@gmail.com>
2023-07-12 17:04:35 -0400

    Added balance fixer, and added disableds for buttons if balance is bad

--- app/home-game-calculator/page.tsx
+++ app/home-game-calculator/page.tsx
@@ -1,41 +1,59 @@
 'use client'
 import { getMinTransfers } from '@/lib/decentrailizedBanker'
 import PlayerRow, { PlayerRowData } from './PlayerRow'
-import { Dispatch, SetStateAction, useState } from "react"
+import { Dispatch, SetStateAction, useEffect, useState } from "react"
 import TransactionsModal, { TransactionData } from './TransactionsModal'
 
 export default function HomeGameCalculator() {
     const [players, setPlayers] = useState([{}, {}] as PlayerRowData[])
     const [showTransactions, setShowTransactions] = useState(false)
     const [transactions, setTransactions] = useState([] as TransactionData[])
+    const [balanceSum, setBalanceSum] = useState(0)
+
+    useEffect(() => {
+        setBalanceSum(
+            players
+                .map(({ cashOut, buyIn }) => cashOut - buyIn)
+                .reduce((acc, balance) => acc + balance, 0)
+        )
+    }, [players])
 
     return (<div className="container-fluid">
         {showTransactions && <TransactionsModal {...{
             transactions,
             onClose: () => setShowTransactions(false)
         }} />}
+        <b>{balanceInformation(balanceSum)}</b>
+        <button
+            role='button'
+            onClick={_ =>
+                calculateTransactions(players, setShowTransactions, setTransactions)
+            }
+            disabled={!isBalanceSumValid(balanceSum)}
+        >Calculate Transactions</button>
+        <button
+            role='button'
+            className='contrast'
+            onClick={_ => setPlayers((oldPlayers) => fixBalances(oldPlayers, balanceSum))}
+            disabled={isBalanceSumValid(balanceSum) || isNaN(balanceSum)}
+        >Fix Balances</button>
         <a
             role='button'
             href=''
             onClick={e => {
                 e.preventDefault()
-                setPlayers([...players, {} as PlayerRowData])
+                setPlayers((oldPlayers) => [...oldPlayers, {} as PlayerRowData])
             }}
         >Add Player</a>
-        <a
-            role='button'
-            href=''
-            onClick={e => {
-                e.preventDefault()
-                calculateTransactions(players, setShowTransactions, setTransactions)
-            }}
-        >Calculate Transactions</a>
         {players.map((playerRowData, index) => (<PlayerRow
             key={index}
             {...playerRowData}
             {...{
                 onRemove() {
-                    setPlayers(players.filter((_, filterInd) => index !== filterInd))
+                    setPlayers((oldPlayers) => [
+                        ...oldPlayers.slice(0, index),
+                        ...oldPlayers.slice(index + 1)
+                    ])
                 },
                 handleInputChange(field, value, type) {
                     const parsedValue = type === 'number' ? Number(value) : value
@@ -55,6 +73,33 @@ export default function HomeGameCalculator() {
             }}
         />))}
     </div>)
+}
+
+function isBalanceSumValid(balanceSum: number): boolean {
+    return balanceSum === 0
+}
+
+function fixBalances(players: PlayerRowData[], balanceSum: number): PlayerRowData[] {
+    const numPlayers = players.length
+    const perPerson = balanceSum / numPlayers
+
+    return players.map(((playerRowData, _) => {
+        playerRowData.cashOut -= perPerson
+
+        return playerRowData
+    }))
+}
+
+function balanceInformation(balanceSum: number): string | null {
+    if (balanceSum > 0) {
+        return `Total is $${balanceSum} over total buyins (example $200 should be in play but $210 are being cashed out)`
+    }
+
+    if (balanceSum < 0) {
+        return `$${Math.abs(balanceSum)} is missing in chips (example $200 should be in play but only $190 are being cashed out)`
+    }
+
+    return 'Cash-out balances look good!'
 }
 
 function calculateTransactions(

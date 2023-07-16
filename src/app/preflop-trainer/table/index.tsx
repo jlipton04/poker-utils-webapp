6a1e6fcc708b6e1f1c5422b9f80b55ac68e7c203
Jacob Lipton <jlipton@clemson.edu>
2023-07-16 18:17:15 -0400

    Added dealer button

--- src/app/preflop-trainer/table/index.tsx
+++ src/app/preflop-trainer/table/index.tsx
@@ -4,6 +4,7 @@ import { Card, getCardString } from '@lib/types/card'
 import './styles.scss'
 import classnames from 'classnames'
 import { Player } from '@lib/types/player'
+import Image from 'next/image'
 
 export type TableProps = {
   holeCards: Player[],
@@ -22,32 +23,40 @@ export default function Table({
 }: TableProps) {
   return (
     <div className="container-fluid">
-      <div className="resultMessage">{result}</div>
-      <div className="table"></div>
+      <div className='resultMessage'>{result}</div>
+      <div className='table'></div>
       <div className={`handed-${holeCards.length}`}>
-        {displayOpenedCards(holeCards[0].holeCards, true)}
-        {holeCards.filter((_, index) => index !== 0).map(({ holeCards, inHand }) => {
+        {displayOpenedCards(holeCards[0].holeCards, holeCards[0].position, true)}
+        {holeCards.filter((_, index) => index !== 0).map(({ holeCards, inHand, position }) => {
           if (!inHand) {
             return
           }
 
-          return openVillanCards ? displayOpenedCards(holeCards) : displayVillanCards()
+          return openVillanCards ? displayOpenedCards(holeCards, position) : displayVillanCards(position)
         })}
       </div>
     </div>
   )
 }
 
-function displayVillanCards(): JSX.Element {
+function displayVillanCards(position: string): JSX.Element {
+  const isButton = position === 'BTN'
+
   return (
     <div className={`${seatClassnames}`}>
-      <img className="holeCards" src="/cards/card_back.png" alt="villan hole cards" />
-      <img className="holeCards" src="/cards/card_back.png" alt="villan hole cards" />
+      <img className='holeCards' src="/cards/card_back.png" alt="villan hole cards" />
+      <img className='holeCards' src="/cards/card_back.png" alt="villan hole cards" />
+      {isButton && getDealerButton()}
     </div>
   )
 }
 
-function displayOpenedCards(cards: Card[], isHero: boolean | undefined = false): JSX.Element {
+function displayOpenedCards(
+  cards: Card[],
+  position: string,
+  isHero: boolean = false,
+) {
+  const isButton = position === 'BTN'
 
   return (
     <div className={`${isHero ? 'hero' : ''} ${seatClassnames}`}>
@@ -55,8 +64,19 @@ function displayOpenedCards(cards: Card[], isHero: boolean | undefined = false):
         const cardName = getCardString(card)
 
         return isHero ? <img className={`card${index}`} src={`/cards/${cardName}.png`} key={cardName} alt={cardName} />
-          : <img className="holeCards" src={`/cards/${cardName}.png`} key={cardName} alt={cardName} />
+          : <img className='holeCards' src={`/cards/${cardName}.png`} key={cardName} alt={cardName} />
       })}
+      {isButton && getDealerButton()}
     </div>
   )
+}
+
+function getDealerButton() {
+  return <Image
+    className='bettingLine'
+    src='/dealerButton.png'
+    alt='Dealer Button'
+    width={50}
+    height={50}
+  />
 }

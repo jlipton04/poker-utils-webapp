f0edd0ee3b64511ca15c76d05a02d9ed19de0acc
Jacob Lipton <jlipton@clemson.edu>
2023-07-17 17:51:01 -0400

    Added README to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,14 @@
+import { ReactMarkdown } from "react-markdown/lib/react-markdown";
+import fs from 'fs'
+
 export default function Home() {
   return (
-    <h2>Poker Utils Home Page</h2>
+    <article>
+      <ReactMarkdown>{getReadme()}</ReactMarkdown>
+    </article>
   )
+}
+
+function getReadme() {
+  return fs.readFileSync('README.md', 'utf8')
 }
